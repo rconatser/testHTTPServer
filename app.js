a4547c1d2078315f82893cb812e5c2eeb7aa0743
10806252@my.uvu.edu <10806252@my.uvu.edu>
2020-01-22 13:34:12 -0700

    routes now working, adding json

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const home = require('./routes/home');
 const api = require('./routes/api');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/api', api);
-app.use('/', home);
+app.use(api);
+app.use(home);
 
 app.use((req, res, next) => {
     res.status(404).send('<h1>Page not found</h1>');
