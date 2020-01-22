b7b2eb6d1be0ab17c98e242c6a7634c7aeff53b8
10806252@my.uvu.edu <10806252@my.uvu.edu>
2020-01-22 13:47:49 -0700

    Stuck on adding json

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const home = require('./routes/home');
 const api = require('./routes/api');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(api);
+app.use(api, bodyParser.json(({number: 'One', data: 'Cats are cool'},{number: 'Two', data: 'Dogs also rule'},{number: 'Three', data: 'Bob the Bunny'})));
 app.use(home);
+
 
 app.use((req, res, next) => {
     res.status(404).send('<h1>Page not found</h1>');
