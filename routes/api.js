6b20b0bd6053fa4f1ddd362548b9fa059a5ed25e
10806252@my.uvu.edu <10806252@my.uvu.edu>
2020-01-22 15:01:18 -0700

    json now works

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,12 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/api', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'api.html'));
+//    res.sendFile(path.join(__dirname, '../', 'views', 'api.html'));
+res.json({
+    one: "Cats are cool",
+    two: "Dogs also rule",
+    three: "Bob is a Bunny"
+});
 });
 
 module.exports = router;
